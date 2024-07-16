493549b31a7adf93204f9527514873da3d3a446b
Omitul <omitul1010@gmail.com>
2024-07-16 21:23:57 +0600

    added advertisement compono advertise

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import Advertisement from "../../components/advertisement/Advertisement";
 import CustomerReview from "../../components/customerReview/CustomerReview";
 import Hero from "../../components/hero/Hero";
 import FeaturedProduct from "../featured/FeaturedProducts";
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <Advertisement></Advertisement>
       <FeaturedProduct></FeaturedProduct>
       <CustomerReview></CustomerReview>
     </div>
